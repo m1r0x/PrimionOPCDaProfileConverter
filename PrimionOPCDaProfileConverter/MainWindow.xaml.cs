a61d22a008a3a2942eb433950aaa3229d47d6987
Michael Stratighiou <michael.srtatighiou@gmail.com>
2019-01-24 12:39:03 +0100

    Initial commit

---
+++ PrimionOPCDaProfileConverter/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+using System.Xml;
+
+namespace PrimionOPCDaProfileConverter
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.CheckFileExists = true;
+            dlg.Filter = "XML Files (*.xml)|*.xml|All Files(*.*)|*.*";
+            dlg.Multiselect = false;
+
+            if (dlg.ShowDialog() != true) { return; }
+
+            XmlDocument XMLdoc = new XmlDocument();
+            try
+            {
+                XMLdoc.Load(dlg.FileName);
+            }
+            catch (XmlException)
+            {
+                MessageBox.Show("The XML file is invalid");
+                return;
+            }
+
+            rtbOpenFile.Text = dlg.FileName;
+            vXMLViewer.xmlDocument = XMLdoc;
+        }
+
+        private void btnClearFile_Click(object sender, RoutedEventArgs e)
+        {
+            rtbOpenFile.Text = string.Empty;
+            vXMLViewer.xmlDocument = null;
+        }
+    }
+}
